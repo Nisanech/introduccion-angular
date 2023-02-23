7f7c8f3c81435656e8dd8d11f9614ded42c145f2
Nisanech <57728976+Nisanech@users.noreply.github.com>
2023-02-22 23:23:57 -0500

    Interpolación componente body

--- src/app/pages/body/body.component.ts
+++ src/app/pages/body/body.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-body',
   templateUrl: './body.component.html',
-  styleUrls: ['./body.component.css']
+  styleUrls: ['./body.component.css'],
 })
-export class BodyComponent {
+export class BodyComponent implements OnInit {
+  //* Declaración de variables
+  cStarter: string = 'STARTER';
+  cSuperPremium: string = 'SUPER PREMIUM';
+  cUltimatePremium: string = 'ULTIMATE PREMIUM';
 
+  pStarter: number = 10
+  pSuperPremium: number = 25
+  pUltimatePremium: number = 34
+
+  emailUno: boolean = false
+  emailDos: boolean = true
+  emailtres: boolean = true
+
+  constructor() {
+    console.log(this.pStarter)
+    console.log(this.pSuperPremium)
+    console.log(this.pUltimatePremium)
+  }
+
+  ngOnInit(): void {}
 }
